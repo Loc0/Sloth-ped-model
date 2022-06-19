448080c69bc2909beb685e5750cc2a3a85899e44
loco <43757529+Loc0@users.noreply.github.com>
2022-06-20 01:17:10 +0200

    initial release

---
+++ sloth/__resource.lua
@@ -0,0 +1,8 @@
+--3D model converted by Loco/Mexicans | NO RESELLING 
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+'peds.meta'
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
